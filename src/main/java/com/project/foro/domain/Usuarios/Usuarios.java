07412ecd36cfd35aba0181a2981846f953fb1d27
juliocastellonmendoza <juliocastellonmendoza@gmail.com>
2023-05-29 13:23:05 -0500

    Se crea la clase para las diferentes Exception

--- src/main/java/com/project/foro/domain/Usuarios/Usuarios.java
+++ src/main/java/com/project/foro/domain/Usuarios/Usuarios.java
@@ -1,4 +1,22 @@
 package com.project.foro.domain.Usuarios;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity(name = "Usuarios")
+@Table(name = "usuarios")
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Getter
 public class Usuarios {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String login;
+    private String clave;
 }
